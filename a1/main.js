14c82ee8e9e3f663b11e7ae7d571a5aafbbc067f
Syntapse <45253268+Syntapse@users.noreply.github.com>
2019-12-08 13:27:04 -0600

    NOT THIS BUT THISS

--- a1/main.js
+++ a1/main.js
@@ -1,4 +1,8 @@
-hw = 'Goodbye World!'
-console.log(hw)
-hw = 'Hello world'
-console.log(hw)
+hw = 'Goodbye World!';
+console.log(hw);
+hw = 'Hello world';
+console.log(hw);
+number = 0;
+for (let number = 0; number < 11; number++) {
+    console.log('number is '+number)
+}
---
+++ a2/main.js
@@ -0,0 +1,9 @@
+butthis = 10
+number = 0
+
+for (let number = 0; number < butthis; number++) {
+    console.log('Not '+number+'th')
+}
+if(number = butthis) {
+    console.log('But '+number+'th')
+}
