8a9228798c2f988e449da37277841c60d6bdf3cd
Syntapse <45253268+Syntapse@users.noreply.github.com>
2019-12-08 13:36:34 -0600

    Update main.js

--- a2/main.js
+++ a2/main.js
@@ -1,7 +1,10 @@
-butthis = 10
-for (let number = 0; number < butthis; number++) {
+function nobt() {
+    butthis = 10
+    for (let number = 1; number < butthis; number++) {
     console.log('Not '+number+'th')
-}
-if(number = butthis) {
+    }
+    if(number = butthis) {
     console.log('But '+number+'th')
+    }   
 }
+nobt();
