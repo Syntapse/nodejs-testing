af95bc7077bbed6fe81f842dbb2ac41937e2416f
Syntapse <45253268+Syntapse@users.noreply.github.com>
2020-01-26 12:51:02 -0600

    json testing

--- json testing/main.js
+++ json testing/main.js
@@ -1,5 +1,10 @@
-var names = {
-    "girls": {
-        "a":"s"
-    }
+var owners = {
+    "a": "Max",
+    "b": "Cathrine"
 }
+var pets = {
+    "a": "Cat",
+    "b": "Parrot"
+}
+
+console.log(owners.a +" owns a "+ pets.a +" and "+ owners.b+ " owns a "+ pets.b)
