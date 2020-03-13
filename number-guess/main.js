3cc40235e82e5535db9a5964072908ef9d29f7b0
Syntapse <45253268+Syntapse@users.noreply.github.com>
2020-03-12 20:01:02 -0500

    Changed Number guess to use prompt-sync!

--- number-guess/main.js
+++ number-guess/main.js
@@ -2,8 +2,8 @@ const prompt = require('prompt-sync')();
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-var i = parseInt(prompt('1 to '))
-var x = parseInt(prompt('what is your guess 1 to '+i))
+var i = parseInt(prompt('0 to '))
+var x = parseInt(prompt('what is your guess 0 to '+i+" "))
 var y = getRandomInt(i+1)
 if(x == y) {
   console.log('correct')
