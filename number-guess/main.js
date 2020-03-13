cfe7efc13fc47cfc0a31768cfcc0d04010a7ddf3
Syntapse <45253268+Syntapse@users.noreply.github.com>
2020-03-12 19:57:48 -0500

    what is going on please help omg im have stroke

--- number-guess/main.js
+++ number-guess/main.js
@@ -1,19 +1,13 @@
+const prompt = require('prompt-sync')();
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  
-  readline.question(`What's your guess 1 through 10?`, (guess) => {
-    if(guess == getRandomInt(10)) {
-        console.log('Good Job!')
-    }
-    else {
-        console.log('Nice try! But incorrect')
-    }
-    process.exit(0);
-  })
+  return Math.floor(Math.random() * Math.floor(max));
+}
+var i = parseInt(prompt('1 to '))
+var x = parseInt(prompt('what is your guess 1 to '+i))
+var y = getRandomInt(i+1)
+if(x == y) {
+  console.log('correct')
+}else{
+  console.log(y)
+}
+process.exit(0);
